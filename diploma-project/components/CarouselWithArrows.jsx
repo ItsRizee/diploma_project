08369d8ecd6fbdfbc6521b4f740afe824f5cc60e
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2023-10-20 02:45:38 +0300

    Fixed: missing key property

--- diploma-project/components/CarouselWithArrows.jsx
+++ diploma-project/components/CarouselWithArrows.jsx
@@ -70,7 +70,7 @@ const CarouselWithArrows = ({ categoryName, listOfItems, autoSlide = false, auto
                 <div className="absolute bottom-4 right-0 left-0">
                     <div className="flex items-center justify-center gap-2">
                         {listOfItems.map((_, i) => (
-                            <div className={`transition-all w-3 h-3 bg-white rounded-full ${curr === i ? "p-2" : "bg-opacity-50"}`} />
+                            <div key={i} className={`transition-all w-3 h-3 bg-white rounded-full ${curr === i ? "p-2" : "bg-opacity-50"}`} />
                         ))}
                     </div>
                 </div>
