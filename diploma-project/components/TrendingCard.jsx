b369a4d361c1a2449bc4c1d97c0321aba2cea5e4
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2023-11-16 10:40:11 +0200

    Dp 6 (#5)
    * Added: favicons
    Added: Collapse
    Fixed: Footer's elements positioning
    Fixed: Light theme
    
    * Fixed: responsiveness and styling of nav bar

--- diploma-project/components/TrendingCard.jsx
+++ diploma-project/components/TrendingCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/future/image";
 
 const TrendingCard = () => {
     return (
-        <div className="card sm:card-side bg-base-100 shadow-2xl border-b-2 sm:border-b-0 border-gray-400 w-full h-full flex">
+        <div className="card sm:card-side bg-base-100 border-b-2 sm:border-b-0 border-gray-400 w-full h-full flex">
             <figure className="relative h-72 sm:h-96 sm:w-96">
                 <Image src="/recommended_ring.jpg" alt="Trending ring" className="object-cover object-center" fill />
             </figure>
